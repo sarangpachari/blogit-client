d028979f9101661b994b04fcc87a2b3bbf7afa6a
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-02-24 17:11:42 +0530

    sample pushing 4

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,3 +1,6 @@
-const SERVER_BASE_URL = "http://localhost:3000"
+// const SERVER_BASE_URL = "http://localhost:3000"
+const SERVER_BASE_URL = "https://blogit-server-slyi.onrender.com"
+
+
 
 export default SERVER_BASE_URL 
