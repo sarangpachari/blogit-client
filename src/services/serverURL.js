ecff083dffb737d60f7618b17b9a13631def9070
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-02-26 17:56:45 +0530

    Server URL updation-2

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,5 +1,5 @@
-const SERVER_BASE_URL = "http://localhost:3000"
-// const SERVER_BASE_URL = "https://blogit-server-slyi.onrender.com"
+// const SERVER_BASE_URL = "http://localhost:3000"
+const SERVER_BASE_URL = "https://blogit-server-slyi.onrender.com"
 
 
 
