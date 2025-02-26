dcefd5fc7d2c87e7d42dfb669bc28f0872cb379e
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-02-26 20:27:39 +0530

    Server URL updation-3

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,23 +1,52 @@
-import React from 'react'
-import SERVER_BASE_URL from '../services/serverURL';
+import React from "react";
+import moment from "moment";
+import SERVER_BASE_URL from "../services/serverURL";
+import { Link } from "react-router-dom";
 
-const PostCard = ({posts}) => {
-    console.log(posts);
-    
+const PostCard = ({ posts }) => {
+  console.log(posts);
+  const formattedTime = moment(posts?.createdAt).fromNow(true);
+
   return (
     <div>
-        <div className="w-full  h-[250px] bg-white flex gap-5">
-          <div className="w-1/3">
-            <img src={`${SERVER_BASE_URL}/${posts?.thumbnail}`} className="w-full h-full object-cover" alt="Thumbnail" />
-          </div>
-          <div className="w-2/3 py-5 px-3">
-          <h1>{`Author : ${posts?.userFullName}`}</h1>
+      <div className="w-full h-full bg-white flex md:flex-row flex-col gap-5">
+        <div className="md:w-1/3">
+          <img
+            src={`${SERVER_BASE_URL}/${posts?.thumbnail}`}
+            className="w-full h-full object-cover"
+            alt="Thumbnail"
+          />
+        </div>
+        <div className="md:w-2/3 pt-5 px-3 flex flex-col justify-between">
+          {/* TITLE AND DESCRIPTION */}
+          <div>
             <h1 className="text-2xl font-semibold">{posts?.title}</h1>
-            <p>{posts?.description}</p>
+            {/* DESCRIPTION WITH READ MORE */}
+            <p className="text-gray-600 mt-2">
+              {posts?.description?.length > 150
+                ? `${posts?.description.slice(0, 150)}... `
+                : posts?.description}
+              {posts?.description?.length > 150 && (
+                <Link
+                  to={`/post/${posts._id}`}
+                  className="text-blue-500"
+                >
+                  Read More
+                </Link>
+              )}
+            </p>
+          </div>
+          {/* AUTHOR AND TIME */}
+          <div className="md:flex md:justify-between md:items-center border-t-2 border-t-gray-200 px-2 py-5 mt-5">
+            <h1>{`Author : ${posts?.userFullName}`}</h1>
+            <p className="text-gray-500 text-sm">
+              Posted {moment(posts?.createdAt).fromNow()}
+            </p>
           </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
